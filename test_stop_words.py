860b615644d57169dbf51bf81e9f7ffdc710e824
Ikram Ali <mrikram1989@gmail.com>
2019-04-05 10:37:01 +0500

    [test] checking combined urdu words not exits in stop-words list.

--- test_stop_words.py
+++ test_stop_words.py
@@ -1,6 +1,7 @@
 # coding: utf8
 """ Test cases """
 
+from urduhack.normalization.character import COMBINE_URDU_CHARACTERS
 from urduhack.urdu_characters import URDU_ALPHABETS
 
 from stop_words import STOP_WORDS
@@ -9,5 +10,9 @@ from stop_words import STOP_WORDS
 def test_stop_words():
     """ Test case"""
     for word in STOP_WORDS:
+        for chars in COMBINE_URDU_CHARACTERS:
+            assert len(chars) == 2
+            assert chars not in word
+
         for character in word:
             assert character in URDU_ALPHABETS, F"Incorrect word: {word} and char: {character}"
