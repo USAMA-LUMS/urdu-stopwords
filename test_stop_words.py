7e6fbd56e561903ce6595299b15a6db53ca1a0da
Ikram Ali <mrikram1989@gmail.com>
2019-04-07 11:49:26 +0500

    [test] new test cases added.

--- test_stop_words.py
+++ test_stop_words.py
@@ -16,3 +16,38 @@ def test_stop_words():
 
         for character in word:
             assert character in URDU_ALPHABETS, F"Incorrect word: {word} and char: {character}"
+
+
+def chunk_it(seq, num):
+    """
+    Converted list into multiple sub list
+    Args:
+        seq: list
+        num: integer
+    Returns:
+        list
+    """
+    avg = len(seq) / float(num)
+    out = []
+    last = 0.0
+
+    while last < len(seq):
+        out.append(seq[int(last):int(last + avg)])
+        last += avg
+
+    return out
+
+
+def sorted_stopwords(words):
+    """Sort the stop words"""
+    print(f"Stop words Count: {len(words)}")
+    stop_words = sorted(words)
+    lists = chunk_it(stop_words, 16)
+
+    for word_list in lists:
+        string_print = ""
+        for line in word_list:
+            string_print = string_print + " " + line.strip()
+
+        print(string_print)
+        string_print = ""
